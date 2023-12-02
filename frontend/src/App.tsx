a30aebd64776d014825cb92eb8b49dc9beea22bc
Saurabh Samanta <saurabhsamanta10@gmail.com>
2023-12-02 20:36:55 +0530

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <header>Amazon</header>
+      <main></main>
+      <footer>All rights reserved</footer>
+    </>
+  );
+}
+
+export default App;
