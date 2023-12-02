e904aadfa979b3dd19891ecd023dc5deebe226b5
Saurabh Samanta <saurabhsamanta10@gmail.com>
2023-12-02 22:45:42 +0530

    Added sample product

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-
+import { sampleProducts } from './data';
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <header>Amazon</header>
-      <main></main>
+      <main>
+        {sampleProducts.map((product) => (
+          <li key={product.slug}>
+            <img
+              src={product.image}
+              alt={product.name}
+              className="product-image"
+            ></img>
+            <h2>{product.name}</h2>
+            <p>${product.price}</p>
+          </li>
+        ))}
+      </main>
       <footer>All rights reserved</footer>
     </>
   );
