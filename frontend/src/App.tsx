e89daf666101a35b764c2b63e6a828ce005d6feb
Saurabh Samanta <saurabhsamanta10@gmail.com>
2023-12-02 23:50:46 +0530

    Added routes of Product & HomePage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
-import { sampleProducts } from './data';
-import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import HomePage from './pages/HomePage';
+import { Outlet } from 'react-router-dom';
 function App() {
   return (
     <div className="d-flex flex-column vh-100">
@@ -22,19 +23,7 @@ function App() {
       </header>
       <main>
         <Container className="mt-3">
-          <Row>
-            {sampleProducts.map((product) => (
-              <Col key={product.slug} sm={6} md={4} lg={3}>
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className="product-image"
-                ></img>
-                <h2>{product.name}</h2>
-                <p>${product.price}</p>
-              </Col>
-            ))}
-          </Row>
+          <Outlet />
         </Container>
       </main>
       <footer>
